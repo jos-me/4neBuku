67585d24aa34f4b5df965322542b2ff14ce7780f
jose-m <jos7.me@gmail.com>
2022-06-23 17:50:38 +0300

    create new element.

--- js/app.js
+++ js/app.js
@@ -126,9 +126,117 @@ let alert = document.querySelector("#submit-alert");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  // getValueOnSubmission();
-  console.log(getValueOnSubmission());
+  let contacts = getValueOnSubmission();
+
+  if (contacts.names != "" && contacts.phone != "") {
+    names = contacts.names;
+    phone = contacts.phone;
+
+    addNewContact(names, phone);
+  }
 });
+
+function addNewContact(text, tel) {
+  //Create new Element
+  let ul = document.createElement("ul");
+  let li = document.createElement("li");
+
+  let header = document.createElement("div");
+  let spanName = document.createElement("span");
+  let spanIcon = document.createElement("span");
+  let iconChevron = document.createElement("i");
+
+  let body = document.createElement("div");
+  let content = document.createElement("div");
+
+  let contentFirstDiv = document.createElement("div");
+  let iconPhone = document.createElement("i");
+  let anchorPhone = document.createElement("a");
+
+  let contentLastDiv = document.createElement("div");
+  let firstDiv = document.createElement("div");
+  let copyDiv = document.createElement("div");
+  let iconCopy = document.createElement("i");
+  let toolTip = document.createElement("div");
+  let toolTipSpan = document.createElement("span");
+
+  let lastDiv = document.createElement("div");
+  let iconInfo = document.createElement("i");
+
+  // Add attributes (classes, ids)
+  ul.id = "accordion";
+  ul.className = "list-group list-group-flush";
+
+  li.id = "item";
+  li.className =
+    "list-group-item fs-6 p-0 d-flex flex-wrap justify-content-between align-items-center";
+
+  header.id = "accordion-header";
+  header.className =
+    "w-100 d-flex justify-content-between align-items-center p-2 bg-light";
+
+  spanIcon.className = "badge text-secondary";
+  iconChevron.className = "fa-solid fa-chevron-down";
+
+  body.id = "accordion-body";
+  body.className = "pt-2 pb-1 shadow-sm rounded-bottom w-100 d-none";
+  content.id = "content";
+  content.className = "ps-3 pe-2 d-flex justify-content-between";
+
+  contentFirstDiv.className = "text-secondary small";
+  iconPhone.className = "fa-solid fa-blender-phone";
+  anchorPhone.href = "tel:"; // TODO: add numbers implicitly
+  anchorPhone.className = "text-secondary text-decoration-none ms-1";
+
+  contentLastDiv.className = "d-flex small position-relative";
+  firstDiv.id = "copy_icon";
+  firstDiv.className = "copy_icon text-secondary p-1 me-3";
+  copyDiv.id = "copy_icon";
+  copyDiv.className = "copy_icon text-secondary p-1 me-3";
+  iconCopy.className = "fa-solid fa-copy";
+  toolTip.className = "d-inline-block position-absolute tool-tip";
+  toolTipSpan.className =
+    "visually-hidden bg-secondary text-white px-2 pb-1 small rounded-1 position-absolute tool-tip_text";
+
+  lastDiv.className = "info_icon text-primary p-1 me-2";
+  iconInfo.id = "info_icon";
+  iconInfo.className = "fa-solid fa-circle-info";
+
+  // Create TextNodes
+  ul.appendChild(li);
+
+  li.appendChild(header);
+  li.appendChild(body);
+
+  header.appendChild(spanName);
+  spanName.appendChild(document.createTextNode(`${text}`));
+  header.appendChild(spanIcon);
+  spanIcon.appendChild(iconChevron);
+
+  body.appendChild(content);
+  content.appendChild(contentFirstDiv);
+  content.appendChild(contentLastDiv);
+
+  contentFirstDiv.appendChild(iconPhone);
+  contentFirstDiv.appendChild(anchorPhone);
+  anchorPhone.appendChild(document.createTextNode(`${tel}`));
+
+  contentLastDiv.appendChild(firstDiv);
+
+  firstDiv.appendChild(copyDiv);
+  copyDiv.appendChild(iconCopy);
+  copyDiv.appendChild(toolTip);
+  toolTip.appendChild(toolTipSpan);
+  toolTipSpan.appendChild(document.createTextNode("copied"));
+
+  contentLastDiv.appendChild(lastDiv);
+  lastDiv.appendChild(iconInfo);
+
+  // insert to dom
+  let container = document.querySelector(".contact-section > .container");
+  let reference = document.querySelector("#reference");
+  container.insertBefore(ul, reference);
+}
 
 function getValueOnSubmission() {
   alertOnSubmission(text, tel);
